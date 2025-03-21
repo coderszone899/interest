bdb772fce973601b1c23df2f3ca79e43a55ce55e
lezboro213 <lezrel.boro@yahoo.com>
2025-03-21 21:14:09 +0000

    Remove styles - refactoring code

--- dist/sources/laguerre.js
+++ dist/sources/laguerre.js
@@ -1,14 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Eval_laguerre = void 0;
-const defs_1 = require("../runtime/defs");
-const stack_1 = require("../runtime/stack");
-const add_1 = require("./add");
-const bignum_1 = require("./bignum");
-const eval_1 = require("./eval");
-const list_1 = require("./list");
-const multiply_1 = require("./multiply");
-const subst_1 = require("./subst");
+var defs_1 = require("../runtime/defs");
+var stack_1 = require("../runtime/stack");
+var add_1 = require("./add");
+var bignum_1 = require("./bignum");
+var eval_1 = require("./eval");
+var list_1 = require("./list");
+var multiply_1 = require("./multiply");
+var subst_1 = require("./subst");
 /*
  Laguerre function
 
@@ -35,10 +35,10 @@ In the "for" loop i = n-1 so the recurrence relation becomes
   (i+1)*L(x,n,k) = (2*i+1-x+k)*L(x,n-1,k) - (i+k)*L(x,n-2,k)
 */
 function Eval_laguerre(p1) {
-    const X = eval_1.Eval(defs_1.cadr(p1));
-    const N = eval_1.Eval(defs_1.caddr(p1));
-    const p2 = eval_1.Eval(defs_1.cadddr(p1));
-    const K = p2 === defs_1.symbol(defs_1.NIL) ? defs_1.Constants.zero : p2;
+    var X = eval_1.Eval(defs_1.cadr(p1));
+    var N = eval_1.Eval(defs_1.caddr(p1));
+    var p2 = eval_1.Eval(defs_1.cadddr(p1));
+    var K = p2 === defs_1.symbol(defs_1.NIL) ? defs_1.Constants.zero : p2;
     stack_1.push(laguerre(X, N, K));
 }
 exports.Eval_laguerre = Eval_laguerre;
@@ -56,7 +56,7 @@ function laguerre2(n, p1, p3) {
     let Y0 = defs_1.Constants.zero;
     let Y1 = defs_1.Constants.one;
     for (let i = 0; i < n; i++) {
-        const result = multiply_1.divide(add_1.subtract(multiply_1.multiply(add_1.add(add_1.subtract(bignum_1.integer(2 * i + 1), p1), p3), Y1), multiply_1.multiply(add_1.add(bignum_1.integer(i), p3), Y0)), bignum_1.integer(i + 1));
+        var result = multiply_1.divide(add_1.subtract(multiply_1.multiply(add_1.add(add_1.subtract(bignum_1.integer(2 * i + 1), p1), p3), Y1), multiply_1.multiply(add_1.add(bignum_1.integer(i), p3), Y0)), bignum_1.integer(i + 1));
         Y0 = Y1;
         Y1 = result;
     }

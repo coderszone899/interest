aae784820d8213225e581c7a4e839aef9c672901
lezboro213 <lezrel.boro@yahoo.com>
2025-03-22 22:14:21 +0000

    Improve README - refactoring code

--- dist/sources/qadd.js
+++ dist/sources/qadd.js
@@ -1,11 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.qadd = void 0;
-const defs_1 = require("../runtime/defs");
-const bignum_1 = require("./bignum");
-const madd_1 = require("./madd");
-const mgcd_1 = require("./mgcd");
-const mmul_1 = require("./mmul");
+var defs_1 = require("../runtime/defs");
+var bignum_1 = require("./bignum");
+var madd_1 = require("./madd");
+var mgcd_1 = require("./mgcd");
+var mmul_1 = require("./mmul");
 //  Add rational numbers
 //
 //  Input:    p1    addend
@@ -16,9 +16,9 @@ function qadd(qadd_frac1, qadd_frac2) {
     // a, qadd_ab, b, qadd_ba, c are all bigNum
     // we are adding the fractions qadd_frac1 + qadd_frac2 i.e.
     // qadd_frac1.q.a/qadd_frac1.q.b + qadd_frac2.q.a/qadd_frac2.q.b
-    const qadd_ab = mmul_1.mmul(qadd_frac1.q.a, qadd_frac2.q.b);
-    const qadd_ba = mmul_1.mmul(qadd_frac1.q.b, qadd_frac2.q.a);
-    const qadd_numerator = madd_1.madd(qadd_ab, qadd_ba);
+    var qadd_ab = mmul_1.mmul(qadd_frac1.q.a, qadd_frac2.q.b);
+    var qadd_ba = mmul_1.mmul(qadd_frac1.q.b, qadd_frac2.q.a);
+    var qadd_numerator = madd_1.madd(qadd_ab, qadd_ba);
     //mfree(qadd_ab)
     //mfree(qadd_ba)
     // zero?
@@ -27,15 +27,15 @@ function qadd(qadd_frac1, qadd_frac2) {
         //mfree(qadd_numerator)
         return defs_1.Constants.zero;
     }
-    const qadd_denominator = mmul_1.mmul(qadd_frac1.q.b, qadd_frac2.q.b);
+    var qadd_denominator = mmul_1.mmul(qadd_frac1.q.b, qadd_frac2.q.b);
     let gcdBetweenNumeratorAndDenominator = mgcd_1.mgcd(qadd_numerator, qadd_denominator);
     //console.log "gcd("+qadd_numerator+","+qadd_denominator+"): " + gcdBetweenNumeratorAndDenominator
     gcdBetweenNumeratorAndDenominator = bignum_1.makeSignSameAs(gcdBetweenNumeratorAndDenominator, qadd_denominator);
     //console.log "qadd qadd_denominator: " + qadd_denominator
     //console.log "qadd gcdBetweenNumeratorAndDenominator: " + gcdBetweenNumeratorAndDenominator
-    const a = mmul_1.mdiv(qadd_numerator, gcdBetweenNumeratorAndDenominator);
-    const b = mmul_1.mdiv(qadd_denominator, gcdBetweenNumeratorAndDenominator);
-    const resultSum = new defs_1.Num(a, b);
+    var a = mmul_1.mdiv(qadd_numerator, gcdBetweenNumeratorAndDenominator);
+    var b = mmul_1.mdiv(qadd_denominator, gcdBetweenNumeratorAndDenominator);
+    var resultSum = new defs_1.Num(a, b);
     //console.log "qadd resultSum.q.a: " + resultSum.q.a
     //console.log "qadd resultSum.q.b: " + resultSum.q.b
     //mfree(qadd_numerator)

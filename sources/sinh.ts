bd98a24e1d655e4ca5d8b52a6ec835e03522121a
lezboro213 <lezrel.boro@yahoo.com>
2025-03-23 00:01:27 +0000

    Fix dependencies - removing deprecated function

--- sources/sinh.ts
+++ sources/sinh.ts
@@ -25,7 +25,7 @@ export function ysinh(p1: U): U {
     return cadr(p1);
   }
   if (isdouble(p1)) {
-    let d = Math.sinh(p1.d);
+    const d = Math.sinh(p1.d);
     if (Math.abs(d) < 1e-10) {
       d = 0.0;
     }

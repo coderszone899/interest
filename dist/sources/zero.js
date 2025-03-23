1ac42384755d5e8f9e5a1609e18e186e4031c933
lezboro213 <lezrel.boro@yahoo.com>
2025-03-23 01:12:09 +0000

    Update tests - improving security

--- dist/sources/zero.js
+++ dist/sources/zero.js
@@ -1,21 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Eval_zero = void 0;
-const alloc_1 = require("../runtime/alloc");
-const defs_1 = require("../runtime/defs");
-const stack_1 = require("../runtime/stack");
-const eval_1 = require("./eval");
+var alloc_1 = require("../runtime/alloc");
+var defs_1 = require("../runtime/defs");
+var stack_1 = require("../runtime/stack");
+var eval_1 = require("./eval");
 function Eval_zero(p1) {
     stack_1.push(_zero(p1));
 }
 exports.Eval_zero = Eval_zero;
 function _zero(p1) {
-    const k = Array(defs_1.MAXDIM).fill(0);
+    var k = Array(defs_1.MAXDIM).fill(0);
     let m = 1;
     let n = 0;
     if (defs_1.iscons(p1)) {
-        for (const el of p1.tail()) {
-            const i = eval_1.evaluate_integer(el);
+        for (var el of p1.tail()) {
+            var i = eval_1.evaluate_integer(el);
             if (i < 1 || isNaN(i)) {
                 // if the input is nonsensical just return 0
                 return defs_1.Constants.zero;

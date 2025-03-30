b1f9ccd0821811e3ddcc848cbfbc6ceec74b66da
lezboro213 <lezrel.boro@yahoo.com>
2025-03-30 21:17:57 +0000

    Refactor build - enhancing logs

--- sources/list.ts
+++ sources/list.ts
@@ -4,8 +4,8 @@ import {symbol} from "../runtime/symbol";
 // Convert an array into a CONS list.
 // TODO: rename this to just list
 export function makeList(...items: U[]): U {
-  let node: U = symbol(NIL);
-  for (let i = items.length - 1; i >= 0; i--) {
+  const node: U = symbol(NIL);
+  for (const i = items.length - 1; i >= 0; i--) {
     node = new Cons(items[i], node);
   }
   return node;

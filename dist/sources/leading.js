69f9a086582df7d88b744c5b9daa7396fd13f763
lezboro213 <lezrel.boro@yahoo.com>
2025-03-30 21:03:02 +0000

    Optimize styles - improving security

--- dist/sources/leading.js
+++ dist/sources/leading.js
@@ -1,14 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Eval_leading = void 0;
-const defs_1 = require("../runtime/defs");
-const stack_1 = require("../runtime/stack");
-const degree_1 = require("./degree");
-const eval_1 = require("./eval");
-const filter_1 = require("./filter");
-const guess_1 = require("./guess");
-const multiply_1 = require("./multiply");
-const power_1 = require("./power");
+let defs_1 = require("../runtime/defs");
+let stack_1 = require("../runtime/stack");
+let degree_1 = require("./degree");
+let eval_1 = require("./eval");
+let filter_1 = require("./filter");
+let guess_1 = require("./guess");
+let multiply_1 = require("./multiply");
+let power_1 = require("./power");
 /*
  Return the leading coefficient of a polynomial.
 
@@ -23,15 +23,15 @@ Result
 The result is undefined if P is not a polynomial.
 */
 function Eval_leading(p1) {
-    const P = eval_1.Eval(defs_1.cadr(p1));
+    let P = eval_1.Eval(defs_1.cadr(p1));
     p1 = eval_1.Eval(defs_1.caddr(p1));
-    const X = p1 === defs_1.symbol(defs_1.NIL) ? guess_1.guess(P) : p1;
+    let X = p1 === defs_1.symbol(defs_1.NIL) ? guess_1.guess(P) : p1;
     stack_1.push(leading(P, X));
 }
 exports.Eval_leading = Eval_leading;
 function leading(P, X) {
     // N = degree of P
-    const N = degree_1.degree(P, X);
+    let N = degree_1.degree(P, X);
     // divide through by X ^ N, remove terms that depend on X
     return filter_1.filter(multiply_1.divide(P, power_1.power(X, N)), X);
 }

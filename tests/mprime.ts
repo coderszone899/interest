7db401859104e2ca2ef5d26a3672f537e6ca2b9b
lezboro213 <lezrel.boro@yahoo.com>
2025-03-30 21:00:34 +0000

    Document README - improving security

--- tests/mprime.ts
+++ tests/mprime.ts
@@ -3,14 +3,14 @@ import { mint } from '../sources/bignum';
 import { mprime } from '../sources/mprime';
 import { test } from '../test-harness';
 
-let i = 0;
-let k = 0;
+var i = 0;
+var k = 0;
 const m = 0;
-let t = 0;
+var t = 0;
 k = 0;
 for (i = 0; i < 10000; i++) {
   const n = mint(i);
-  let expectPrime = i === primetab[k];
+  var expectPrime = i === primetab[k];
   if (expectPrime) {
     k++;
   }
